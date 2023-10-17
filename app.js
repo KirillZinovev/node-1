deccd7fced814126f5c6f446c9ac821e96a0de78
= <=>
2023-10-17 15:23:22 +0300

    commit

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.get("/as", function (req, res) {
   res.end("hello");
   console.log("ok");
 });
+app.post("/as", function (req, res) {
+  console.log(req.body);
+});
 
 app.listen(port, () => {
   console.log("listening on port: " + port);
